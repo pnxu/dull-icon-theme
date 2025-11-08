186c6cb40780d0df33bc333629d1c2ddd2d17950
pnxu <fcaavv@gmail.com>
2025-11-08 01:18:41 -0300

    fix: update packages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "pnxu-icons" extension will be documented in this file.
+All notable changes to the "dull-icon-theme" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- extension.js
+++ extension.js
@@ -12,16 +12,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pnxu-icons" is now active!');
+	console.log('Congratulations, your extension "dull-icon-theme" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('pnxu-icons.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('dull-icon-theme.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from pnxu-icons!');
+		vscode.window.showInformationMessage('Hello World from dull-icon-theme!');
 	});
 
 	context.subscriptions.push(disposable);
